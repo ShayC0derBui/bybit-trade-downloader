6071e5cb08101595e8c9f1a7557bc4ec3d8ec4e0
ShayC0derBui <sinhaprem636@gmail.com>
2023-12-24 20:24:47 +0530

    Update base URL for future trading

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def insert_rows(data, cursor, connection):
 
 # Replace this with the base URL of your directory listing
 # 'base_url_spot':'https://public.bybit.com/spot/', 'base_url_future': 'https://public.bybit.com/trading/',
-url = { 'base_url_spot':'https://public.bybit.com/spot/'}
+url = {'base_url_future': 'https://public.bybit.com/trading/'}
 
 # Check if the "temp" directory exists and delete it if it does
 if os.path.exists("temp"):
